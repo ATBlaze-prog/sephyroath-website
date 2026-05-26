9c127bd00b7c8fc5a5700c00860fb29abe69b745
ATBlaze-prog <taran.aeronricxel.zamora@gmail.com>
2026-05-27 01:33:37 +0800

    Synchronize ESLint and finalize Prisma v7 configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   datasource: {
-    // Vercel automatically maps your Environment Variables
-    url: process.env.DATABASE_URL as string,
+    url: process.env.DATABASE_URL!,
   },
 });
