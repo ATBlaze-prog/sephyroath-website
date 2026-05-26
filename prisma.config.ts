ac2cb1c97188ac4c4886951640d470c7bb3f2007
ATBlaze-prog <taran.aeronricxel.zamora@gmail.com>
2026-05-27 03:46:11 +0800

    Update scripts to use local binary

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-// prisma.config.ts
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  datasource: {
-    // This loads the DATABASE_URL from your environment variables
-    url: process.env.DATABASE_URL as string,
-  },
-});
